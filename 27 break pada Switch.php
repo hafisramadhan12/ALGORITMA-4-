98584a687f23c1a35136feeeebe6d2e13f16f10a
hafidznasution <rhafis709@gmail.com>
2024-08-31 13:57:15 +0700

    break pada switch

---
+++ 27 break pada Switch.php
@@ -0,0 +1,19 @@
+<?php
+$buah = "apel";
+
+switch ($buah) {
+    case "mangga":
+        echo "Ini mangga";
+        break;
+    case "apel":
+        echo "Ini durian";
+        // Break disini menghentikan switch
+        break;
+    case "pisang":
+        echo "Ini pisang";
+        break;
+    default:
+        echo "Buah tidak dikenal";
+}
+
+print_r("<p><strong><i>By M HAFIDZ RAMADHAN NST!</strong>");
