31a7ea59174a37a4007bf9762251967608aee74d
hafidznasution <rhafis709@gmail.com>
2024-08-31 13:40:31 +0700

    forench dengan mendapatkan key

---
+++ 24 foreach dengan mendapatkan key dari array .php
@@ -0,0 +1,9 @@
+
+<?php
+$harga = array("apel" => 2000, "pokat" => 1000, "durian" => 2700);
+
+foreach ($harga as $kunci => $nilai) {
+    echo "Harga dari " . $kunci . " adalah " . $nilai . "<br>";
+}
+
+print_r("<p><strong><i>By M HAFIDZ RAMADHAN NST!</strong>");
