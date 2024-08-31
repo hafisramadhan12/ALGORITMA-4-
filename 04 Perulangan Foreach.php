5887b88f24d434cbc9c56eaf74406528fd4402a9
hafidznasution <rhafis709@gmail.com>
2024-08-31 11:13:13 +0700

    Perulangan Foreach

---
+++ 04 Perulangan Foreach.php
@@ -0,0 +1,20 @@
+<?php
+$books = [
+    "Panduan Belajar PHP untuk Pemula",
+    "Membangun Aplikasi Web dengan PHP",
+    "Tutorial PHP dan MySQL",
+    "Membuat Chat Bot dengan PHP"
+];
+
+echo "<h5>Judul Buku PHP,algoritma:</h5>";
+echo "<ul>";
+foreach($books as $buku){
+    echo "<li>$buku</li>";
+}
+echo "</ul>";
+
+
+print_r("<p><strong><i>By M HAFIDZ RAMADHAN NST!</strong>");
+
+?>
+
