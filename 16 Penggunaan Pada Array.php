099207254de10d89127a25ee2056551a4d156c89
hafidznasution <rhafis709@gmail.com>
2024-08-31 12:42:03 +0700

    penggunaan pada aray

---
+++ 16 Penggunaan Pada Array.php
@@ -0,0 +1,8 @@
+<?php
+$arr = array("udang", "kepiting", "ikan");
+
+for ($i = 0; $i < count($arr); $i++) {
+    echo $arr[$i] . " ";
+}
+
+print_r("<p><strong><i>By M HAFIDZ RAMADHAN NST!</strong>");
