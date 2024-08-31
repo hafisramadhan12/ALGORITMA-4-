1ae4ca70e0c722fe7a563eac21d0da26e2a07a5f
hafidznasution <rhafis709@gmail.com>
2024-08-31 12:29:23 +0700

    cara membuat tabel dengan perulangan

---
+++ 14 Cara Membuat Tabel dengan perulangan.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "<table border=1>";
+for($i=1; $i <=3; $i++){
+      echo "<tr>";
+      for($j=1; $j<=5; $j++){
+            echo "<td>";
+            echo $i.$j;
+            echo "</td>";
+      }
+}
+echo "</table>";
+
+print_r("<p><strong><i>By M HAFIDZ RAMADHAN NST!</strong>");
+
+?>
