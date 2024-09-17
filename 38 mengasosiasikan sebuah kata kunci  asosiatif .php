eada006e41c06f5a000341fd5eb6d97b5bc469cb
hafidznasution <rhafis709@gmail.com>
2024-09-17 10:27:43 +0700

    mengasosiasikan sebuah kata kunci asosiatif

---
+++ 38 mengasosiasikan sebuah kata kunci  asosiatif .php
@@ -0,0 +1,10 @@
+<?php
+$email["subjek"] = "Apa Kabar?";
+$email["pengirim"] = "rhafis@gmail.com";
+$email["isi"] = " hii Apa kabar hafis? sudah lama kita tidak berjumpa";
+
+echo "<pre>";
+print_r($email);
+echo "</pre>";
+
+echo "<p><strong><i>By HAFIS RAMADHAN</strong>";
