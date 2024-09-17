9221a25a1bc8fc60f2c77c5ebe1c1fb708459c03
hafidznasution <rhafis709@gmail.com>
2024-09-17 10:21:25 +0700

    aray asosiatif

---
+++ 37 array asosiatif .php
@@ -0,0 +1,14 @@
+<?php
+// membuat array asosiatif
+$artikel = [
+    "judul" => "Belajar Pemrograman PHP dan algoritma",
+    "penulis" => "m hafidz ramadhan",
+    "view" => 123
+];
+
+// mencetak isi array assosiatif
+echo "<h2>".$artikel["judul"]."</h2>";
+echo "<p>oleh: ".$artikel["penulis"]."</p>";
+echo "<p>View: ".$artikel["view"]."</p>";
+
+echo "<p><strong><i>By HAFIS RAMADHAN</strong>";
