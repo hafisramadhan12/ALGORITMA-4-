d14eff829de50916e67c57eaf01d981f44610028
hafidznasution <rhafis709@gmail.com>
2024-09-17 10:42:56 +0700

    assosiatif array

---
+++ 42 associative array .php
@@ -0,0 +1,7 @@
+<?php
+$kendaraan = array('jenis' => 'Mobil', 'merk' => 'lamborgini', 'tipe' => 'veneno');
+foreach ($kendaraan as $key => $val) {
+	echo ucfirst($key) . ': ' . $val . '<br/>';
+}
+
+echo "<p><strong><i>By HAFIS RAMADHAN</strong>";
