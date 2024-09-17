d373fa6082e625f481e3cd0ded728f8ddc5256c1
hafidznasution <rhafis709@gmail.com>
2024-09-17 10:17:31 +0700

    menambahkan indeks

---
+++ 36 menambahkan pada indeks yang sudah memiliki isi .php
@@ -0,0 +1,19 @@
+<?php
+// membuat array
+$user = [
+    "pisss",
+    "muhar",
+    "hafis"
+];
+
+// mengisi array pada indek ke-1 ("muhar")
+$user[1] = "arjun";
+
+// mencetak isi array
+echo "<pre>";
+print_r($user);
+echo "</pre>";
+
+echo "<p><strong><i>By HAFIS RAMADHAN</strong>";
+
+?>
